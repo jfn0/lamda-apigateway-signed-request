eaeff187f91cca9c342a5dab9a30720150d83a15
jfn0 <jfrederic.nault@gmail.com>
2020-02-01 21:21:09 -0500

    implement get function using axios and aws4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const Test = () => {
-    console.log("test1git");
+import Axios from "axios";
+
+const aws4 = require("aws4");
+const URL = require("url");
+
+const get = async (url) => {
+    let url = URL.parse(url, {});
+
+    let opts = {
+        host: url.hostname,
+        path: url.pathname,
+    };
+
+    aws4.sign(opts);
+
+    return await Axios.get(url, { headers: opts.headers })
 }
 
-export default Test;
+export default get;
